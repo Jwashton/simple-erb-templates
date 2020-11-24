d19c6415931fe623cbde6ddfbcba04cf1f8b3bc0
William Ashton <William@AshtonFam.org>
2020-11-24 14:17:27 -0500

    Ensure dist directory exists, cleanup output

--- build.rb
+++ build.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require 'erb'
+require 'fileutils'
 
 LAYOUT = ERB.new(File.read('./templates/layout.html.erb'))
 PAGE_DIR = File.join('.', 'pages')
@@ -23,11 +24,14 @@ end
 # Various functions for compiling one or more files
 module Util
   def self.render_file(filename)
-    puts "Rendering #{filename}"
+    FileUtils.mkdir_p(DIST_DIR)
+
+    new_name = filename.delete_suffix('.erb')
+    puts "Rendering #{new_name}"
 
     result = Page.new(filename).render
 
-    File.write(File.join(DIST_DIR, filename.delete_suffix('.erb')), result)
+    File.write(File.join(DIST_DIR, new_name), result)
   end
 
   def self.render_all
